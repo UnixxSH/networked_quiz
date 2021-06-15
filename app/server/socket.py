a22ed9e81e2cd8a354eca6770e65a8abe5de6851
UnixxSH <17497055+UnixxSH@users.noreply.github.com>
2021-06-15 13:26:47 +0200

    initial app commit - server/client's socket

---
+++ app/server/socket.py
@@ -0,0 +1,11 @@
+## Mods
+import socketserver
+from collections import namedtuple
+from fl_networking_tools import get_binary, send_binary
+
+'''
+Commands:
+SEND - used to send a question to client
+FEEDBACK - used to send the feedback to client
+'''
+
