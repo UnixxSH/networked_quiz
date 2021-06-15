c2baa229495ea1357b5901f33978f88e17f51469
UnixxSH <17497055+UnixxSH@users.noreply.github.com>
2021-06-15 13:28:29 +0200

    socket

--- app/client/socket.py
+++ app/client/socket.py
@@ -1,1 +1,24 @@
 ## Mods
+import socket
+from fl_networking_tools import get_binary, send_binary
+
+'''
+Responses
+LIST YOUR RESPONSE CODES HERE
+1 - Question
+'''
+
+## Playing ?
+playing = True
+
+## Bind socket and connect to server
+quiz_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+quiz_server.connect(("127.0.0.1", 2065))
+
+## Request for question
+send_binary(quiz_server, ["QUESTION", ""])
+
+while playing:
+    for response in get_binary(quiz_server):
+        if response[0] == 1:
+            print(response[1])
--- app/server/socket.py
+++ app/server/socket.py
@@ -5,7 +5,23 @@ from fl_networking_tools import get_binary, send_binary
 
 '''
 Commands:
-SEND - used to send a question to client
-FEEDBACK - used to send the feedback to client
+PLACE YOUR COMMANDS HERE
+QUESTION - Used to request a question from the server
 '''
 
+## Question model
+Question = namedtuple('Question', ['q', 'answer'])
+
+## Questions
+q1 = Question("Expand the acronym ALU", "Arithmetic Logic Unit")
+
+## Handler socket class
+class QuizGame(socketserver.BaseRequestHandler):
+    def handle(self):
+        for command in get_binary(self.request):
+            if command[0] == "QUESTION":
+                send_binary(self.request, (1, q1.q))
+
+## Start server, bind socket
+quiz_server = socketserver.TCPServer(('127.0.0.1', 2065), QuizGame)
+quiz_server.serve_forever()
